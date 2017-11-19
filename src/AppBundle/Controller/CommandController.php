39b788306097758b048e46a051764e04f140360d
bacquette <nadantoine1220@hotmail.fr>
2017-11-19 22:07:35 +0100

    changing stuff to test the site

--- src/AppBundle/Controller/CommandController.php
+++ src/AppBundle/Controller/CommandController.php
@@ -162,7 +162,7 @@ class CommandController extends Controller
         $parameters['command'] = $command;
 
         $proposedForm = $this->createFormBuilder()
-            ->add('prix_propose', IntegerType::class, array('grouping' => true, 'data' => $command->getEstimatedPrice()))
+            ->add('prix_propose', IntegerType::class, array('data' => $command->getEstimatedPrice()))
             ->add('save', SubmitType::class, array('label' => 'Proposer un prix'))
             ->getForm();
 
