8f095019518a73f18b622b3bc05ee226321faa44
antoinebacquet <nadantoine1220@hotmail.com>
2018-01-27 22:31:36 +0100

    changing the join discord

--- src/AppBundle/Discord/DiscordConfig.php
+++ src/AppBundle/Discord/DiscordConfig.php
@@ -16,13 +16,13 @@ class DiscordConfig
 
     public static $cliendSecret = 'vC1nVx-h938V4Txzspd56RvvIrUdu8dw';
 
-    public static $redirectURI = "http://plapcorp.com/discord/redirect/";
+    public static $redirectURI = "http://localhost/discord/redirect/";
 
     public static $loginURI = "https://discordapp.com/oauth2/authorize?response_type=code";
 
     public static $tokenURI = "https://discordapp.com/api/v6/oauth2/token";
 
-    public static $token = 'NDA2Njc3MDc1NTQ2NjAzNTMx.DU2bSw.W-OFEOG6XNNTDBb_H4rYbqn_NtM';
+    public static $token = 'MzU1NzMwMTcxNDg5MTU3MTMz.DJ7s-g.QEdQwTgGqZMabJ72Q3BYjOim5zQ';
 
     public static $webhook_url = 'https://discordapp.com/api/webhooks/383371839298207765/MvOKVEt8VpBRxpJOxb9jxW3gP5OKMrjOHSPfkPWbQeIw9eUCuPzX9YmuMsozJ-K1vlKK';
 }
